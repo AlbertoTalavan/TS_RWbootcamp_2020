2f2d0deacc0cc98fc582707799277d1a81737186
AlbertoTalavan <albertotalavan@gmail.com>
2020-05-29 21:10:27 +0200

    Initial commit just to test everything works fine git-ting from Xcode

---
+++ Week01/01-rgbColorPicker/01-rgbColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-rgbColorPicker
+//
+//  Created by Alberto Talaván on 29/05/2020.
+//  Copyright © 2020 Alberto Talavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
