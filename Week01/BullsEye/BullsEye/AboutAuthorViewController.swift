296d2960dda07beab9b3999580b25809b7c97a95
AlbertoTalavan <albertotalavan@gmail.com>
2020-05-29 19:33:46 +0200

    uploaded BullsEy to the github repo

---
+++ Week01/BullsEye/BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Alberto Talaván on 28/05/2020.
+//  Copyright © 2020 Alberto Talavan. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+   }
+   
+   @IBAction func close() {
+      dismiss(animated: true)
+   }
+   
+}
