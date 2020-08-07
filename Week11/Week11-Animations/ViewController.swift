0f74f986fa14a64896a78db5f1c1199277065109
Alberto <albertotalavan@gmail.com>
2020-08-07 21:46:23 +0200

    Week11 initial commit

---
+++ Week11/Week11-Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week11-Animations
+//
+//  Created by Alberto Talaván on 07/08/2020.
+//  Copyright © 2020 Alberto Talavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Week11/Week11_README.md
@@ -0,0 +1,1 @@
+# Week 11 (Monday 3 August to Sunday 9 August)
