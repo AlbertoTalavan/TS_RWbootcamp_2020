8b4ac086d8d680d6f75ab5602f29a50b30457ced
Alberto <albertotalavan@gmail.com>
2020-08-07 22:58:45 +0200

    changing menu constraint values

--- Week11/Week11-Animations/ViewController.swift
+++ Week11/Week11-Animations/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var playButton: UIButton!
+  @IBOutlet weak var squareColourButton: UIButton!
+  @IBOutlet weak var squareSizeButton: UIButton!
+  @IBOutlet weak var squareTranslationButton: UIButton!
+  
+  private var menuIsOpen = false
+  @IBOutlet private var colorLeadingConstraint: NSLayoutConstraint!
+  @IBOutlet private var translationTrailingConstraint: NSLayoutConstraint!
+  @IBOutlet private var sizeBottomConstraint: NSLayoutConstraint!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    setButtons()
   }
 
+  @IBAction func toggleMenu(_ sender: UIButton) {
+    menuIsOpen.toggle()
+    colorLeadingConstraint.constant = menuIsOpen ? 68 : 162.5
+    
+    translationTrailingConstraint.constant = menuIsOpen ? 68 : 162.5
+    
+    sizeBottomConstraint.constant = menuIsOpen ? 44 : -50
+    
+  }
+  
 
+}
+
+private extension ViewController {
+  func setButtons() {
+    let buttons: [UIButton]
+    buttons = [playButton, squareColourButton, squareSizeButton, squareTranslationButton]
+    
+    buttons.forEach {
+      $0.layer.cornerRadius = $0.frame.width / 2
+      $0.tintColor = UIColor.white
+    }
+    buttons[0].backgroundColor = .systemOrange
+    buttons[1].backgroundColor = .systemRed
+    buttons[2].backgroundColor = .systemPurple
+    buttons[3].backgroundColor = .systemTeal
+  }
+  
+  
 }
 
