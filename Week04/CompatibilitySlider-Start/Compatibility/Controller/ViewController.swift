bd6c0e1378bc6a59cc46ff9f9d72a3e302d3fff9
Alberto Talaván <albertotalavan@gmail.com>
2020-10-05 04:51:52 +0200

    Update ViewController.swift

--- Week04/CompatibilitySlider-Start/Compatibility/Controller/ViewController.swift
+++ Week04/CompatibilitySlider-Start/Compatibility/Controller/ViewController.swift
@@ -2,9 +2,9 @@
 //  ViewController.swift
 //  CompatibilitySlider-Start
 //
-//  Created by Jay Strawn on 6/16/20.
+//  Created by Alberto Talaván on 6/16/20.
 
-//  Copyright © 2020 Jay Strawn. All rights reserved.
+//  Copyright © 2020 Alberto Talaván. All rights reserved.
 //
 
 import UIKit
